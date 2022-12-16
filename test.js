41304b20388fb0f1eaba917f5b5e15a21f8c787c
Code To Crack <72027864+PrathamAyush@users.noreply.github.com>
2022-12-16 13:36:22 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+
+function login() {
+if(document.getElementById("uid").value==="admin@12345" && document.getElementById("password").value==="admin12345"){
+    alert("Login Success");
+    window.location.href = ("index.html");
+}else{
+    alert("Incorrect user id and password");
+}
+return false;
+}
